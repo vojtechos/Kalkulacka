1ffa78efce0475d754001807652c865cb0abd50a
Vojtěch Vít <vojtech.vit@student.spsmb.cz>
2021-09-16 14:20:41 +0200

    First commit test

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Zadej cislo");
+        Double a = sc.nextDouble();
+        System.out.println("zadej cislo");
+        Double b = sc.nextDouble();
+        switch (args[0]) {
+            case "s":
+                System.out.println(a + b);
+                break;
+            case "o":
+                System.out.println(a - b);
+                break;
+            case "n":
+                System.out.println(a * b);
+                break;
+            case "d":
+                System.out.println(a / b);
+                break;
+            default:
+                System.out.println("Neplatny argument");
+        }
+    }
+}
